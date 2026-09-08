aaec4eead79ddf6f6ad1cdfd4288cb503ea1660b
Aleksander Czop <alecz0.code@gmail.com>
2026-09-08 11:08:40 +0200

    Refactor transforms.py

--- src/transforms.py
+++ src/transforms.py
@@ -11,11 +11,9 @@ def get_train_transforms():
         LoadImaged(keys=["image", "label"]),
         EnsureChannelFirstd(keys=["image", "label"]),
         Orientationd(keys=["image", "label"], axcodes="RAS", labels=None),
-        
-        # 1. Tworzymy 3 kanały BraTS (to one dają Bool)
+
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         
-        # 2. KLUCZOWY KROK: Zamieniamy Bool na Float32
         CastToTyped(keys=["image", "label"], dtype=(np.float32, np.float32)),
         
         NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
@@ -39,10 +37,8 @@ def get_val_transforms():
         Orientationd(keys=["image", "label"], axcodes="RAS", labels=None),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         
-        # Tutaj też rzutujemy na float, ze względu na błąd w fazie validacji
         CastToTyped(keys=["image", "label"], dtype=(np.float32, np.float32)),
 
-        # Unikanie 
         DivisiblePadd(keys=["image", "label"], k=16),
         
         NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
